83f07366eb263c82d95e0ed7320849a5ce675ec7
John Tom Jr <johntjr.22@gmail.com>
2020-03-07 01:28:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,8 @@ end
 def monopoly_with_third_tierok  
   ok =  {:railroads => {
   :pieces => 4, 
-  :rent_in_dollars => {
+  :rent_in_dollars => 
+  {
     :one_piece_owned =>25,
     :two_pieces_owned => 50,
     :three_pieces_owned => 100,
