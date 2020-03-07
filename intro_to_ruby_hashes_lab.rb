585a56c6f02f968cbc8fd1e2d3c9a9e50a7fb3fa
John Tom Jr <johntjr.22@gmail.com>
2020-03-07 01:45:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,8 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tierok  
- monopoly =  {:railroads => 
+ monopoly =  {
+   :railroads => 
  { :pieces => 4,
   :rent_in_dollars => 
   {
