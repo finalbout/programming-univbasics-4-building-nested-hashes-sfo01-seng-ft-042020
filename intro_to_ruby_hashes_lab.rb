a0665f678bb5f21698bd04282633acbde3390034
John Tom Jr <johntjr.22@gmail.com>
2020-03-07 01:42:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,7 @@ def monopoly_with_third_tierok
   {
     :reading_railroad => {},
     :pennsylvania_railroad => {},
-    :b_and_o_raildroad =>{},
+    :b_and_o_raildroad => {},
     :shortline_railroad => {}
   }
 }
